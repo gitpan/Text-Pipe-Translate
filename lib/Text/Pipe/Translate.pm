33cee8e5dfef9b6c085bc9791572856afb4ce6cd
Marcel Grünauer <marcel@cpan.org>
2010-03-29 22:56:15 +0000

    Import of MARCEL/Text-Pipe-Translate-1.100890 from CPAN.
    gitpan-cpan-distribution: Text-Pipe-Translate
    gitpan-cpan-version:      1.100890
    gitpan-cpan-path:         MARCEL/Text-Pipe-Translate-1.100890.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Text/Pipe/Translate.pm
+++ lib/Text/Pipe/Translate.pm
@@ -1,10 +1,12 @@
-package Text::Pipe::Translate;
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
+
+package Text::Pipe::Translate;
+our $VERSION = '1.100890';
+# ABSTRACT: Translate text from one language to another
 use Lingua::Translate;
-our $VERSION = '0.05';
-use base qw(Text::Pipe::Base);
+use parent qw(Text::Pipe::Base);
 __PACKAGE__->mk_scalar_accessors(qw(from to translators));
 use constant DEFAULTS => (translators => {},);
 
@@ -19,13 +21,18 @@ sub filter_single {
     $self->get_translator->translate($input);
 }
 1;
+
+
 __END__
-
-
+=pod
 
 =head1 NAME
 
 Text::Pipe::Translate - Translate text from one language to another
+
+=head1 VERSION
+
+version 1.100890
 
 =head1 SYNOPSIS
 
@@ -43,167 +50,49 @@ L<I18N::LangTags>.
 
 =head1 METHODS
 
-=over 4
-
-=item C<clear_from>
-
-    $obj->clear_from;
-
-Clears the value.
-
-=item C<clear_to>
-
-    $obj->clear_to;
-
-Clears the value.
-
-=item C<clear_translators>
-
-    $obj->clear_translators;
-
-Clears the value.
-
-=item C<from>
-
-    my $value = $obj->from;
-    $obj->from($value);
-
-A basic getter/setter method. If called without an argument, it returns the
-value. If called with a single argument, it sets the value.
-
-=item C<from_clear>
-
-    $obj->from_clear;
-
-Clears the value.
-
-=item C<to>
-
-    my $value = $obj->to;
-    $obj->to($value);
-
-A basic getter/setter method. If called without an argument, it returns the
-value. If called with a single argument, it sets the value.
-
-=item C<to_clear>
-
-    $obj->to_clear;
-
-Clears the value.
-
-=item C<translators>
-
-    my $value = $obj->translators;
-    $obj->translators($value);
-
-A basic getter/setter method. If called without an argument, it returns the
-value. If called with a single argument, it sets the value.
-
-=item C<translators_clear>
-
-    $obj->translators_clear;
-
-Clears the value.
-
-=item C<get_translator>
+=head2 get_translator
 
 Constructs a translator object for the current settings of C<from()> and
 C<to()> and caches it using the C<translators()> accessor.
 
-=item C<filter_single>
+=head2 filter_single
 
 Takes a string and translates it according to the current settings of
 C<from()> and C<to()>.
 
-=back
+=head1 INSTALLATION
 
-Text::Pipe::Translate inherits from L<Text::Pipe::Base>.
-
-The superclass L<Text::Pipe::Base> defines these methods and functions:
-
-    new(), bit_or(), filter(), init()
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    mk_abstract_accessors(), mk_array_accessors(), mk_boolean_accessors(),
-    mk_class_array_accessors(), mk_class_hash_accessors(),
-    mk_class_scalar_accessors(), mk_concat_accessors(),
-    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
-    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
-    mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor()
-
-The superclass L<Class::Accessor::Constructor> defines these methods and
-functions:
-
-    _make_constructor(), mk_constructor(), mk_constructor_with_dirty(),
-    mk_singleton_constructor()
-
-The superclass L<Data::Inherited> defines these methods and functions:
-
-    every_hash(), every_list(), flush_every_cache_by_key()
-
-The superclass L<Class::Accessor::Constructor::Base> defines these methods
-and functions:
-
-    STORE(), clear_dirty(), clear_hygienic(), clear_unhygienic(),
-    contains_hygienic(), contains_unhygienic(), delete_hygienic(),
-    delete_unhygienic(), dirty(), dirty_clear(), dirty_set(),
-    elements_hygienic(), elements_unhygienic(), hygienic(),
-    hygienic_clear(), hygienic_contains(), hygienic_delete(),
-    hygienic_elements(), hygienic_insert(), hygienic_is_empty(),
-    hygienic_size(), insert_hygienic(), insert_unhygienic(),
-    is_empty_hygienic(), is_empty_unhygienic(), set_dirty(),
-    size_hygienic(), size_unhygienic(), unhygienic(), unhygienic_clear(),
-    unhygienic_contains(), unhygienic_delete(), unhygienic_elements(),
-    unhygienic_insert(), unhygienic_is_empty(), unhygienic_size()
-
-The superclass L<Tie::StdHash> defines these methods and functions:
-
-    CLEAR(), DELETE(), EXISTS(), FETCH(), FIRSTKEY(), NEXTKEY(), SCALAR(),
-    TIEHASH()
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Text-Pipe-Translate>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://search.cpan.org/dist/Text-Pipe-Translate/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Text-Pipe-Translate/>.
 
-=head1 AUTHORS
+The development version lives at
+L<http://github.com/hanekomu/Text-Pipe-Translate/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+=head1 AUTHOR
+
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2009 by the authors.
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
--- lib/Text/Pipe/Translate/Babelfish.pm
+++ lib/Text/Pipe/Translate/Babelfish.pm
@@ -1,25 +1,36 @@
-package Text::Pipe::Translate::Babelfish;
+use 5.008;
 use strict;
 use warnings;
+
+package Text::Pipe::Translate::Babelfish;
+our $VERSION = '1.100890';
+# ABSTRACT: Translate text using Babelfish
 use LWP::UserAgent;
-our $VERSION = '0.05';
-use base qw(Text::Pipe::Translate);
+use parent qw(Text::Pipe::Translate);
 
 sub init {
     my $self = shift;
     $self->SUPER::init(@_);
     Lingua::Translate::config(
-        back_end      => "Babelfish",
+        back_end => "Babelfish",
+
         # babelfish_uri => 'http://babelfish.altavista.com/tr?',
-        ua            => LWP::UserAgent->new,
+        ua => LWP::UserAgent->new,
     );
 }
 1;
+
+
 __END__
+=pod
 
 =head1 NAME
 
 Text::Pipe::Translate::Babelfish - Translate text using Babelfish
+
+=head1 VERSION
+
+version 1.100890
 
 =head1 SYNOPSIS
 
@@ -34,107 +45,43 @@ uses L<Lingua::Translate>'s Babelfish backend.
 
 =head1 METHODS
 
-=over 4
-
-=item C<init>
+=head2 init
 
 Instructs L<Lingua::Translate> to use the C<Babelfish> backend.
 
-=back
+=head1 INSTALLATION
 
-Text::Pipe::Translate::Babelfish inherits from L<Text::Pipe::Translate>.
-
-The superclass L<Text::Pipe::Translate> defines these methods and
-functions:
-
-    clear_from(), clear_to(), clear_translators(), filter_single(), from(),
-    from_clear(), get_translator(), to(), to_clear(), translators(),
-    translators_clear()
-
-The superclass L<Text::Pipe::Base> defines these methods and functions:
-
-    new(), bit_or(), filter()
-
-The superclass L<Class::Accessor::Complex> defines these methods and
-functions:
-
-    mk_abstract_accessors(), mk_array_accessors(), mk_boolean_accessors(),
-    mk_class_array_accessors(), mk_class_hash_accessors(),
-    mk_class_scalar_accessors(), mk_concat_accessors(),
-    mk_forward_accessors(), mk_hash_accessors(), mk_integer_accessors(),
-    mk_new(), mk_object_accessors(), mk_scalar_accessors(),
-    mk_set_accessors(), mk_singleton()
-
-The superclass L<Class::Accessor> defines these methods and functions:
-
-    _carp(), _croak(), _mk_accessors(), accessor_name_for(),
-    best_practice_accessor_name_for(), best_practice_mutator_name_for(),
-    follow_best_practice(), get(), make_accessor(), make_ro_accessor(),
-    make_wo_accessor(), mk_accessors(), mk_ro_accessors(),
-    mk_wo_accessors(), mutator_name_for(), set()
-
-The superclass L<Class::Accessor::Installer> defines these methods and
-functions:
-
-    install_accessor()
-
-The superclass L<Class::Accessor::Constructor> defines these methods and
-functions:
-
-    _make_constructor(), mk_constructor(), mk_constructor_with_dirty(),
-    mk_singleton_constructor()
-
-The superclass L<Data::Inherited> defines these methods and functions:
-
-    every_hash(), every_list(), flush_every_cache_by_key()
-
-The superclass L<Class::Accessor::Constructor::Base> defines these methods
-and functions:
-
-    STORE(), clear_dirty(), clear_hygienic(), clear_unhygienic(),
-    contains_hygienic(), contains_unhygienic(), delete_hygienic(),
-    delete_unhygienic(), dirty(), dirty_clear(), dirty_set(),
-    elements_hygienic(), elements_unhygienic(), hygienic(),
-    hygienic_clear(), hygienic_contains(), hygienic_delete(),
-    hygienic_elements(), hygienic_insert(), hygienic_is_empty(),
-    hygienic_size(), insert_hygienic(), insert_unhygienic(),
-    is_empty_hygienic(), is_empty_unhygienic(), set_dirty(),
-    size_hygienic(), size_unhygienic(), unhygienic(), unhygienic_clear(),
-    unhygienic_contains(), unhygienic_delete(), unhygienic_elements(),
-    unhygienic_insert(), unhygienic_is_empty(), unhygienic_size()
-
-The superclass L<Tie::StdHash> defines these methods and functions:
-
-    CLEAR(), DELETE(), EXISTS(), FETCH(), FIRSTKEY(), NEXTKEY(), SCALAR(),
-    TIEHASH()
+See perlmodinstall for information and options on installing Perl modules.
 
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Text-Pipe-Translate>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://search.cpan.org/dist/Text-Pipe-Translate/>.
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see
+L<http://search.cpan.org/dist/Text-Pipe-Translate/>.
 
-=head1 AUTHORS
+The development version lives at
+L<http://github.com/hanekomu/Text-Pipe-Translate/>.
+Instead of sending patches, please fork this project using the standard git
+and github infrastructure.
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+=head1 AUTHOR
+
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2009 by the authors.
+This software is copyright (c) 2007 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
 
